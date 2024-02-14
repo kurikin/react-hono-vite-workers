8ad4ceaad49512aed1c53c63b5926b51dc29eff5
kurikin <riku.code0309@gmail.com>
2024-02-14 18:33:04 +0900

    Move index.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: "src/server.ts",
+      output: {
+        entryFileNames: "_worker.js",
+      },
+    },
+  },
   plugins: [react()],
-})
+});
